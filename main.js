ee162e170e436f795c154d02f75a22246fa04ef3
hawkey <ashawkey1999@gmail.com>
2019-05-28 16:40:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 (function () {
-	const MAX_DELAY = 60 // 切课时的最大延迟，单位：秒
+	const MAX_DELAY = 10 // max delay in second
+	const CHECK_INTERVAL = 1 // loop interval in second
 
 	const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time))
 
@@ -9,46 +10,49 @@
 
 		while (true) {
 			console.log('正在检查')
-			const delay = Math.floor(Math.random() * MAX_DELAY * 1000) + 1000
-
+			
 			if (!/1\.5/.test($('.speedBox').attr('style'))) {
 				console.log('提升到1.5倍速')
 				$('.speedTab15').click()
 			}
 
-			if (!/liuchang/.test($('.definiBox').attr('style'))) {
+			else if (!/liuchang/.test($('.definiBox').attr('style'))) {
 				console.log('更改画质')
 				$('.line1bq').click()
 			}
 
-			if ($('.volumeBox').find('.passVolume').width() != 0) {
+			else if ($('.volumeBox').find('.passVolume').width() != 0) {
 				console.log('静音')
 				$('.volumeIcon').click()
 			}
 			
-			if ($('.playButton').length > 0) {
-				console.log('点击播放')
+			else if ($('.playButton').length > 0) {
+				const delay = Math.floor(Math.random() * MAX_DELAY * 1000) + 100
+				console.log('发现暂停, 将在' + delay / 10000 + ' 秒后播放')
+				await sleep(delay / 10)
 				$('.playButton').click()
 			}
 
-			if ($('.popbtn_cancel')[0]) {
-				console.log('发现弹题，将在' + delay / 1000 + ' 秒后点击关闭')
-				await sleep(delay / 2)
+			else if ($('.popbtn_cancel')[0]) {
+				const delay1 = Math.floor(Math.random() * MAX_DELAY * 1000) + 100
+				const delay2 = Math.floor(Math.random() * MAX_DELAY * 1000) + 100
+				console.log('发现弹题，将在' + (delay1 + delay2) / 1000 + ' 秒后点击关闭')
+				await sleep(delay1)
 				const iframe = document.getElementById('tmDialog_iframe').contentWindow
 				iframe.document.querySelector('.answerOption label').click()
-				await sleep(delay / 2)
+				await sleep(delay2)
 				$('.popbtn_cancel').click()
 			}
 
-			if ($('.current_play').find('.progressbar').width() == $('.current_play').find('.progressbar_box').width()) {
-				console.log('本节完成，' + delay / 1000 + ' 秒后将切到下一课')
-				await sleep(delay)
+			else if ($('.current_play').find('.progressbar').width() == $('.current_play').find('.progressbar_box').width()) {
+				const delay = Math.floor(Math.random() * MAX_DELAY * 1000) + 100
+				console.log('本节完成，' + delay / 5000 + ' 秒后将切到下一课')
+				await sleep(delay / 5)
 				$('.current_play').nextAll('.video')[0].click()
 			}
-
-			await sleep(1000)
+		
+			await sleep(CHECK_INTERVAL * 1000)
 		}
 	}
-
 	start()
 }())
